6eb3e528c0b0c5705eab8b064761837786e9e226
Vladimir Tsynbusov <vladimirtsynbusov@gmail.com>
2025-05-22 20:29:16 +0300

    initial commit

---
+++ src/masks.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+
+def get_mask_card_number(card_number: Optional[int] = None) -> str:
+    """Функция маскировки номера банковской карты"""
+    if len(str(card_number)) != 16:
+        return "Некорректный номер карты"
+
+    return f"{str(card_number)[:4]} {str(card_number)[4:6]}** **** {str(card_number)[-4:]}"
+
+
+def get_mask_account(account_number: Optional[int] = None) -> str:
+    """Функция маскировки номера банковского счета"""
+    if len(str(account_number)) != 20:
+        return "Некорректный номер счета"
+
+    return f"**{str(account_number)[-4:]}"
