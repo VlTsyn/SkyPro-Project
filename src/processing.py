d698f915038273b3508c47d3e7187c6eec7c0c3e
Vladimir Tsynbusov <vladimirtsynbusov@gmail.com>
2025-06-06 02:31:13 +0300

    add htmlcov and fixes

--- src/processing.py
+++ src/processing.py
@@ -33,7 +33,14 @@ def sort_by_date(transactions_list: list[dict[str, Any]], desc: bool = True) -> 
         return "Некорректная дата"
     return sorted(filtered_list, key=lambda i: i["date"], reverse=desc)
 
-print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED'},
-           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def test_mask_number():
@@ -10,18 +10,23 @@ def test_mask_number():
 @pytest.fixture
 def card_error():
     return "Некорректный номер карты"
-@pytest.mark.parametrize("number",
-                         [0,
-                          1234567890,
-                          12345678901234567890,
-                          "1234 9012 1234 1234",
-                          "asdfghasdqwertyq",
-                          "123456789o123456",
-                          "1234()7_9+-2a4s6",
-                          "123456789  23456",
-                          "",
-                          ])
-def test_len_card_number(number,card_error):
+
+
+@pytest.mark.parametrize(
+    "number",
+    [
+        0,
+        1234567890,
+        12345678901234567890,
+        "1234 9012 1234 1234",
+        "asdfghasdqwertyq",
+        "123456789o123456",
+        "1234()7_9+-2a4s6",
+        "123456789  23456",
+        "",
+    ],
+)
+def test_len_card_number(number, card_error):
     assert get_mask_card_number(number) == card_error
 
 
@@ -32,16 +37,21 @@ def test_mask_account():
 @pytest.fixture
 def account_error():
     return "Некорректный номер счета"
-@pytest.mark.parametrize("number",
-                         [0,
-                          1234567890,
-                          1234567890123456789012345,
-                          "1234 9012 1234 1234 ",
-                          "asdfghasdqwertyqqwer",
-                          "123456789o123456qwer",
-                          "1234()7_9+-2a4s6,.=!",
-                          "ewfesdgfdhgj",
-                          "",
-                          ])
-def test_len_account_number(number,account_error):
+
+
+@pytest.mark.parametrize(
+    "number",
+    [
+        0,
+        1234567890,
+        1234567890123456789012345,
+        "1234 9012 1234 1234 ",
+        "asdfghasdqwertyqqwer",
+        "123456789o123456qwer",
+        "1234()7_9+-2a4s6,.=!",
+        "ewfesdgfdhgj",
+        "",
+    ],
+)
+def test_len_account_number(number, account_error):
     assert get_mask_account(number) == account_error
